e8929aa4704bf4abb52b6019dcb10df10bdd3bd7
Ronald-silva <ronaldsilvajob@yahoo.com>
2025-12-31 18:07:42 -0300

    REFATORADO TODA PAGINA

--- script.js
+++
@@ -1,9 +0,0 @@
-// Efeito de rolagem suave para os botões
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
